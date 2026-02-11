80547835594e6a0955a15cf89e8c5cd3920ebe3f
Hamad Almansouri <60302091@udst.edu.qa>
2026-02-11 21:01:25 +0300

    Fixed business layer and added persistance require

--- business.js
+++ business.js
@@ -1,60 +1,66 @@
+const persist = require("./persistence")
+
 /**
  * This function attempts to assign a shift to an employee. This function checks to ensure
- * that the employee exists, the shift exists, and that the combination employee/shift has 
+ * that the employee exists, the shift exists, and that the combination employee/shift has
  * not already been recorded.
- * 
- * The function currently returns string messages indicating whether the operation was successful
- * or why it failed.  A serious improvement would be to use exceptions; this will be refactored
- * at a later time.
- * 
- * @param {string} empId 
- * @param {string} shiftId 
- * @returns {string} A message indicating the problem of the word "Ok"
+ *
+ * @param {string} empId
+ * @param {string} shiftId
+ * @returns {string} returns message of confirmation "OK" or the problem
  */
 async function assignShift(empId, shiftId) {
     // check that empId exists
-    let employee = await findEmployee(empId)
+    let employee = await persist.findEmployee(empId)
     if (!employee) {
         return "Employee does not exist"
     }
+
     // check that shiftId exists
-    let shift = await findShift(shiftId)
+    let shift = await persist.findShift(shiftId)
     if (!shift) {
         return "Shift does not exist"
     }
+
     // check that empId,shiftId doesn't exist
-    let assignment = await findAssignment(empId, shiftId)
+    let assignment = await persist.findAssignment(empId, shiftId)
     if (assignment) {
         return "Employee already assigned to shift"
     }
+
     // add empId,shiftId into the bridge
-    await addAssignment(empId, shiftId)
+    await persist.addAssignment(empId, shiftId)
     return "Ok"
 }
 
 /**
- * Get a list of shiftIDs for an employee.
- * @param {string} empId 
- * @returns {Array<{string}>}
+ * Return a list of all employees.
+ * @returns {Array<{ employeeId: string, name: string, phone: string }>}
+ */
+async function getAllEmployees() {
+    return await persist.getAllEmployees()
+}
+
+/**
+ * Add a new employee record.
+ * @param {{name:string, phone:string}} emp
+ */
+async function addEmployee(emp) {
+    await persist.addEmployeeRecord(emp)
+}
+
+/**
+ * Get a list of shifts for an employee.
+ * @param {string} empId
+ * @returns {Array<{shiftId:string, date:string, startTime:string, endTime:string}>}
  */
 async function getEmployeeShifts(empId) {
-    let rawData = await fs.readFile('assignments.json')
-    assignmentList = JSON.parse(rawData)
-    let shiftIds = []
-    for (let asn of assignmentList) {
-        if (asn.employeeId == empId) {
-            shiftIds.push(asn.shiftId)
-        }
-    }
+    return await persist.getEmployeeShifts(empId)
+}
 
-    rawData = await fs.readFile('shifts.json')
-    shiftList = JSON.parse(rawData)
-    let shiftDetails = []
-    for (let sh of shiftList) {
-        if (shiftIds.includes(sh.shiftId)) {
-            shiftDetails.push(sh)
-        }
-    }
-
-    return shiftDetails
+module.exports = {
+    assignShift,
+    getAllEmployees,
+    addEmployee,
+    getEmployeeShifts
 }
