7f02a9811098a85ac89d0a8dd5cfc4964522d353
Hamad Almansouri <60302091@udst.edu.qa>
2026-03-27 18:01:03 +0300

    migration first step to create empty arrays for employees in shifts

---
+++ transform.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require("mongodb")
+const CONNECTION_STRING = "mongodb://60302091:G3TeCNnCT7HoFoo2@cluster0-shard-00-00.wrr68.mongodb.net:27017,cluster0-shard-00-01.wrr68.mongodb.net:27017,cluster0-shard-00-02.wrr68.mongodb.net:27017/infs3201_winter2026?ssl=true&authSource=admin&replicaSet=atlas-2te6sv-shard-0&retryWrites=true&w=majority"
+const DATABASE_NAME = "infs3201_winter2026";
+
+let db;
+
+async function addEmptyEmployeeArray() {
+    const client = new MongoClient(CONNECTION_STRING);
+    await client.connect();
+    db = client.db(DATABASE_NAME);
+
+    const shifts = db.collection("shifts")
+    await shifts.updateMany({}, {$set: {employees: []}})
+
+    console.log("Updated")
+}
+
+addEmptyEmployeeArray()
