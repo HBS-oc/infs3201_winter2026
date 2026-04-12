348447d297512e4fee6f4a4f86bf03efe32064d8
Hamad Almansouri <60302091@udst.edu.qa>
2026-04-12 16:24:40 +0300

    Fixed logging email for assignment requriements

--- emailSystem.js
+++ emailSystem.js
@@ -7,13 +7,19 @@ const transport = nodemailer.createTransport({host: '127.0.0.1', port: 25})
 async function sendTwoFactorCode(email, code){
     console.log("Sending 2FA email")
 
-    try{
-    transport.sendMail({
-        from:"no-reply@shifts.com",
+    let message = {
         to: email,
         subject: "2FA code",
-        text: `Your 2FA code is ${generateOTP()}`
-    })
+        text: `Your 2FA code is ${code}`
+    }
+
+    console.log("to: " + message.to)
+    console.log("subject: " + message.subject)
+    console.log("body: " + message.text)
+    
+    try{
+        await transport.sendMail({from:"no-reply@shifts.com",
+            ...message})
     } catch (err){
         console.log("Email sent has failed!", err.message)
     }
@@ -21,12 +27,19 @@ async function sendTwoFactorCode(email, code){
 
 async function suspiciousActivity(email){
     try{
-    transport.sendMail({
-        from:"no-reply@shifts.com",
-        to: email,
-        subject: "Suspicious Activity Detected",
-        text: `Invalid attempts has been detected on your account`
-    })
+        message = {
+            to: email,
+            subject: "Suspicious Activity Detected",
+            text: `Invalid attempts has been detected on your account`
+        }
+        console.log("to: " + message.to)
+        console.log("subject: " + message.subject)
+        console.log("body: " + message.text)
+        
+        transport.sendMail({
+            from:"no-reply@shifts.com",
+            ...message
+        })
     } catch (err){
         console.log("Email sent has failed!", err.message)
     }
